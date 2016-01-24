0513cf1d62c857785aaa63b466656223fee0d47c
Will Cassella <wcassella@gmail.com>
2016-01-24 14:45:40 -0700

    Updated example game

---
+++ Games/ExampleGame/include/ExampleGame/FPSCharacter.h
@@ -0,0 +1,39 @@
+// FPSCharacter.h - Copyright 2013-2015 Will Cassella, All Rights Reserved
+#pragma once
+
+#include <Core/Memory/PointerBase.h>
+#include <Engine/Components/CameraComponent.h>
+#include <Engine/Components/CapsuleComponent.h>
+#include <Engine/Components/StaticMeshComponent.h>
+#include "config.h"
+
+class EXAMPLEGAME_API FPSCharacter : public Entity
+{
+	///////////////////////
+	///   Information   ///
+public:
+
+	REFLECTABLE_CLASS
+	EXTENDS(Entity)
+
+	//////////////////////
+	///   Components   ///
+public:
+
+	weak_ptr<CameraComponent> View;
+	weak_ptr<CapsuleComponent> Capsule;
+
+	///////////////////
+	///   Methods   ///
+protected:
+
+	void OnSpawn() override;
+
+	///////////////////
+	///   Actions   ///
+public:
+
+	void Move(Vec2 direction);
+	void Look(Vec2 direction);
+	void Fire();
+};
---
+++ Games/ExampleGame/source/FPSCharacter.cpp
@@ -0,0 +1,45 @@
+// FPSCharacter.cpp - Copyright 2013-2015 Will Cassella, All Rights Reserved
+
+#include <Engine/World.h>
+#include "../include/ExampleGame/FPSCharacter.h"
+
+//////////////////////
+///   Reflection   ///
+
+BUILD_REFLECTION(FPSCharacter)
+.Field("View", &FPSCharacter::View, "")
+.Field("Capsule", &FPSCharacter::Capsule, "");
+
+///////////////////
+///   Methods   ///
+
+void FPSCharacter::OnSpawn()
+{
+	Base::OnSpawn();
+
+	Capsule = &this->Connect<CapsuleComponent>();
+	View = &this->Attach<CameraComponent>();
+
+	GetWorld().Events.Bind("Move", *this, &FPSCharacter::Move);
+	GetWorld().Events.Bind("Look", *this, &FPSCharacter::Look);
+	GetWorld().Events.Bind("Click", *this, &FPSCharacter::Fire);
+}
+
+///////////////////
+///   Actions   ///
+
+void FPSCharacter::Move(Vec2 direction)
+{
+	this->Translate(Vec3(direction.X, 0, -direction.Y)/10);
+}
+
+void FPSCharacter::Look(Vec2 direction)
+{
+	this->RotateGlobal(Vec3::Up, direction.X);
+	View->Rotate(Vec3::Right, direction.Y);
+}
+
+void FPSCharacter::Fire()
+{
+	// Spawn something
+}
