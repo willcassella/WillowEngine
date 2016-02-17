7666ab736b5b9cfa25040c060d2c872a04b1051f
Will Cassella <wcassella@gmail.com>
2016-02-16 17:30:08 -0700

    Updated travis again

---
+++ travis-packages.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+# If we're compiling for GCC, download and install latest version
+if (( "$CC" == "gcc" && "$CXX" == "g++" ))
+then
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+    sudo apt-get update -q
+    sudo apt-get install gcc-5 -y -qq
+    sudo apt-get install g++-5 -y -qq
+    export CC=gcc-5
+    export CXX=g++-5
+# Do the same for Clang
+elif (( "$CC" == "clang" && "$CXX" == "clang++" ))
+then
+    sudo add-apt-repository llvm-toolchain-trusty -y
+    sudo apt-get update -q
+    sudo apt-get install clang-3.7
+    export CC=clang-3.7
+    export CXX=clang++-3.7
+# We're neither compiling for GCC or Clang, not sure what happened
+else
+    echo "Unknown compiler configuration"
+    exit 1
+fi
+
+# Download engine dependencies
+sudo apt-get install -y -qq libfreeimage-dev libpugixml-dev libglew-dev libbullet-dev
+
+# Download and compile GLFW, since it's not available in a Ubuntu repository on 14.04
+sudo apt-get install -y -qq xorg-dev libglu1-mesa-dev
+wget https://github.com/glfw/glfw/archive/3.1.2.tar.gz
+tar -xzvf 3.1.2.tar.gz
+cd glfw-3.1.2 && cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF ./ && make && sudo make install
