4a40183eb679c2abbd24224803e1a95754bff634
Will Cassella <wcassella@gmail.com>
2016-02-18 22:28:36 -0700

    Added some experimental static (compile-time) tests, this needs more work though

--- Tests/CoreTest/source/main.cpp
+++ Tests/CoreTest/source/main.cpp
@@ -1,15 +1,17 @@
 // main.cpp - Copyright 2013-2016 Will Cassella, All Rights Reserved
 
-#include <Core/Core.h>
+#include <Core/Application.h>
 #include <Core/Math/Vec4.h>
+#include <Core/Reflection/StructInfo.h>
+#include <Core/Functional/EnumeratorView.h>
 #include <Core/Containers/Nullable.h>
 #include <Core/Containers/Union.h>
 #include <Core/Test/Test.h>
 
 void FormatTest(WTest::Context& context)
 {
-	String test = Format("UniquePtr<@>{ <@, @, @> }", TypeOf<Vec3>().GetName(), 1.f, 2.5f, 3.5f);
-	String fTest = fFormat("UniquePtr<@>{ <@, @, @> }", TypeOf<Vec3>().GetName(), 1.f, 2.5f, 3.5f);
+	String test = Format("TestPtr<@>{ <@, @, @> }", TypeOf<Vec3>().GetName(), 1.f, 2.5f, 3.5f);
+	String fTest = fFormat("TestPtr<@>{ <@, @, @> }", TypeOf<Vec3>().GetName(), 1.f, 2.5f, 3.5f);
 
 	context.AssertEquals(test, fTest);
 }
@@ -62,6 +64,47 @@ void EnumerationViewTest(WTest::Context& context)
 	sum = 0;
 	func([&sum](int value) { sum += value; return sum == 6 ? EnumeratorResult::Break : EnumeratorResult::Continue; });
 	context.AssertEquals(6, sum);
+}
+
+////////////////////////
+///   Static Tests   ///
+
+namespace TypeTraitsTest
+{
+#	define COPY_CONST_ERROR "'stde::copy_const' does not work as intended on this compiler."
+	static_assert(std::is_same<int, stde::copy_const_t<int, int>>::value, COPY_CONST_ERROR);
+	static_assert(std::is_same<const int, stde::copy_const_t<const int, int>>::value, COPY_CONST_ERROR);
+	static_assert(std::is_same<const int, stde::copy_const_t<const int, const int>>::value, COPY_CONST_ERROR);
+	static_assert(std::is_same<const int, stde::copy_const_t<const volatile int, int>>::value, COPY_CONST_ERROR);
+#	undef COPY_CONST_ERROR
+
+#	define COPY_VOLATILE_ERROR "'stde::copy_volatile' does not work as intended on this compiler."
+	static_assert(std::is_same<int, stde::copy_volatile_t<int, int>>::value, COPY_VOLATILE_ERROR);
+	static_assert(std::is_same<volatile int, stde::copy_volatile_t<volatile int, int>>::value, COPY_VOLATILE_ERROR);
+	static_assert(std::is_same<volatile int, stde::copy_volatile_t<volatile int, volatile int>>::value, COPY_VOLATILE_ERROR);
+	static_assert(std::is_same<volatile int, stde::copy_volatile_t<const volatile int, int>>::value, COPY_VOLATILE_ERROR);
+#	undef COPY_VOLATILE_ERROR
+
+#	define COPY_VC_ERROR "'stde::copy_cv' does not work as intended on this compiler."
+	static_assert(std::is_same<int, stde::copy_cv_t<int, int>>::value, COPY_VC_ERROR);
+	static_assert(std::is_same<const int, stde::copy_cv_t<const int, int>>::value, COPY_VC_ERROR);
+	static_assert(std::is_same<const int, stde::copy_cv_t<const int, const int>>::value, COPY_VC_ERROR);
+	static_assert(std::is_same<volatile int, stde::copy_cv_t<volatile int, int>>::value, COPY_VC_ERROR);
+	static_assert(std::is_same<volatile int, stde::copy_cv_t<volatile int, volatile int>>::value, COPY_VC_ERROR);
+	static_assert(std::is_same<const volatile int, stde::copy_cv_t<const volatile int, int>>::value, COPY_VC_ERROR);
+	static_assert(std::is_same<const volatile int, stde::copy_cv_t<const volatile int, const int>>::value, COPY_VC_ERROR);
+	static_assert(std::is_same<const volatile int, stde::copy_cv_t<const volatile int, volatile int>>::value, COPY_VC_ERROR);
+	static_assert(std::is_same<const volatile int, stde::copy_cv_t<const volatile int, const volatile int>>::value, COPY_VC_ERROR);
+#	undef COPY_VC_ERROR
+
+#	define COPY_REF_ERROR "'stde::copy_ref' does not work as intended on this compiler."
+	static_assert(std::is_same<int, stde::copy_ref_t<int, int>>::value, COPY_REF_ERROR);
+	static_assert(std::is_same<int&, stde::copy_ref_t<int&, int>>::value, COPY_REF_ERROR);
+	static_assert(std::is_same<int&, stde::copy_ref_t<int&, int&>>::value, COPY_REF_ERROR);
+	static_assert(std::is_same<int&, stde::copy_ref_t<const int&, int>>::value, COPY_REF_ERROR);
+	static_assert(std::is_same<int&&, stde::copy_ref_t<int&&, int>>::value, COPY_REF_ERROR);
+	static_assert(std::is_same<int&&, stde::copy_ref_t<int&&, int&&>>::value, COPY_REF_ERROR);
+#	undef COPY_REF_ERROR
 }
 
 int main()
