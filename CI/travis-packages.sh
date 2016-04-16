029781291eaba355dd465f37d4c7eae49165fe3a
Will Cassella <wcassella@gmail.com>
2016-04-15 19:04:15 -0600

    Building Bullet from source instead of package, hopefully this will work

--- CI/travis-packages.sh
+++ CI/travis-packages.sh
@@ -21,10 +21,15 @@ else
 fi
 
 # Download engine dependencies
-sudo apt-get install -y -qq libfreeimage-dev libpugixml-dev libglew-dev libbullet-dev
+sudo apt-get install -y -qq libfreeimage-dev libpugixml-dev libglew-dev
 
 # Download and compile GLFW, since it's not available in a Ubuntu repository on 14.04
 sudo apt-get install -y -qq xorg-dev libglu1-mesa-dev
 wget https://github.com/glfw/glfw/archive/3.1.2.tar.gz
 tar -xzvf 3.1.2.tar.gz
-cd glfw-3.1.2 && cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF ./ && make && sudo make install
+cd glfw-3.1.2 && cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF ./ && make && sudo make install && cd ..
+
+# Download and compile Bullet, since the version they have is outdated
+wget https://github.com/bulletphysics/bullet3/archive/2.83.7.tar.gz
+tar -xzvf 2.83.7.tar.gz
+cd bullet-2.83.7 && cmake ./ -DBUILD_BULLET3=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_CPU_DEMOS=OFF -DBUILD_UNIT_TESTS=OFF && make && sudo make install && cd ..
