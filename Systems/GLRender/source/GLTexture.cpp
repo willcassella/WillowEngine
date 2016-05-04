beaf3cecc8512ae52bdcc9c5395ddc69fac95ead
Will Cassella <wcassella@gmail.com>
2016-05-04 17:11:57 -0600

    Added mipmapping and anistropic filtering to textures

--- Systems/GLRender/source/GLTexture.cpp
+++ Systems/GLRender/source/GLTexture.cpp
@@ -18,11 +18,16 @@ namespace willow
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-		// Set filtering parameters, I should learn more about this
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		// Set sampling parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 8);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 8.f);
 
+		// Load the image
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, image.get_width(), image.get_height(), 0, GL_BGRA, GL_UNSIGNED_BYTE, image.get_bitmap());
+		glGenerateMipmap(GL_TEXTURE_2D);
 	}
 
 	GLTexture::GLTexture(GLTexture&& move)
